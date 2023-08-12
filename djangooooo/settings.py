a0df81ff7c3a40f5ca183b13a6ace25058dc2b89
riadkabir45 <riadkabir45@gmail.com>
2023-08-12 22:57:58 +0600

    wtf2

--- djangooooo/settings.py
+++ djangooooo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qigg665#f+=fon0jlp-%^zl@#th8k9@f1k3d0sqs9zn_-#y=_5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','https://testdrive-830b2a1d5c73.herokuapp.com/']
 
 
 # Application definition
